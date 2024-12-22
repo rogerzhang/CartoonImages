ec8ad0bf477eed151c5e0fe63a0416924f49b701
rogerzhang <694703286@qq.com>
2024-12-22 20:28:55 +0800

    非会员只能用前两个特效

--- CartoonImages/Views/Components/ImageModelTypeSelectionView.swift
+++ CartoonImages/Views/Components/ImageModelTypeSelectionView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ImageModelTypeSelectionView: View {
     @State private var currentIndex = mainStore.state.imageState.currentModelType?.id ?? "0"
-    @State private var beautyEnabled = true
+    @State private var beautyEnabled = false
     @State private var showImagePicker = false
+    @State private var showPayment = false
     @State var showCameraView: Bool = false
     
     @EnvironmentObject var viewModel: ImageProcessingViewModel
@@ -36,7 +37,13 @@ struct ImageModelTypeSelectionView: View {
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             
             Button(action: {
-                showCameraView = true
+                if viewModel.isSubscribed || currentIndex == "0" || currentIndex == "1" {
+                    showCameraView = true
+                    showPayment = false
+                } else {
+                    showPayment = true
+                    showCameraView = false
+                }
             }) {
                 Text("TAKE_PHOTO".localized)
                     .foregroundColor(.white) // 设置文字颜色
@@ -61,6 +68,16 @@ struct ImageModelTypeSelectionView: View {
                 selectedImage: $viewModel.selectedImage,
                 isPresented: $showCameraView,
                 beautyEnabled: $beautyEnabled
+            )
+        }
+        .sheet(isPresented: $showPayment) {
+            PaymentView(
+                showPaymentAlert: .constant(false),
+                paymentIsProcessing: $viewModel.paymentIsProcessing,
+                showPaymentError: .constant(false),
+                isSubscribed: $viewModel.isSubscribed,
+                paymentError: nil,
+                handlePayment: {}
             )
         }
     }
