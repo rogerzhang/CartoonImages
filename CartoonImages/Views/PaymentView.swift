5b8e5527ddfaaccb88adba0660c5c2d0cf4567e1
rogerzhang <694703286@qq.com>
2025-01-04 20:34:46 +0800

    App review issue

--- CartoonImages/Views/PaymentView.swift
+++ CartoonImages/Views/PaymentView.swift
@@ -18,148 +18,155 @@ struct PaymentView: View {
 
     var body: some View {
         ZStack {
-            VStack {
-                HStack {
+            ScrollView {
+                VStack {
                     HStack {
-                        Image(systemName: self.isSubscribed ? "crown.fill" : "crown")
-                            .font(.system(size: 38))
-                            .foregroundColor(self.isSubscribed ? .yellow : .gray)
-                        VStack {
-                            Text(self.isSubscribed ? "SUBSCRIBED".localized : "NOT_SUBSCRIBED".localized)
-                                .font(.headline)
-                                .foregroundStyle(.primary)
-                            if self.isSubscribed {
-                                let dataString = PaymentService.shared.formartedExpirationDate()
-                                Text("EXPIRES_ON".localizedFormat(dataString))
-                                    .font(.system(size: 14))
-                                    .foregroundStyle(.secondary)
-                            } else {
-                                Text("UPGRADE_TO_VIP".localized)
-                                    .font(.system(size: 14))
-                                    .foregroundStyle(.secondary)
-                            }
-                        }
-                    }
-                    .padding(.leading, 20)
-                    Spacer()
-                    
-                    Image("viplogo")
-                        .padding(.trailing, 20)
-                }
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("MEMBER_STATUS".localized)
-                            .font(.headline)
-                            .foregroundStyle(
-                                            LinearGradient(
-                                                colors: [.init(hex: 0xABED3B), .init(hex: 0x61DFC2)],
-                                                startPoint: .leading,
-                                                endPoint: .trailing
-                                            )
-                            )
-                            .padding(.bottom, 0)
-                    
-                        ZStack(alignment: .leading) {
-                            Image("vipstate")
-                                .frame(width: 175, height: 114)
-                            
-                            VStack(alignment: .leading, spacing: 5) {
-                                HStack {
-                                    Image("diamon")
-                                    Text("MEMBER_TYPE".localized)
-                                        .font(.system(size: 10))
-                                        .foregroundColor(.init(hex: 0xEEA47D))
-                                }
-                                .padding(.vertical, 5)
-                                .padding(.horizontal, 10)
-                                .background(Color.black)
-                                .cornerRadius(15)
-                                
+                        HStack {
+                            Image(systemName: self.isSubscribed ? "crown.fill" : "crown")
+                                .font(.system(size: 38))
+                                .foregroundColor(self.isSubscribed ? .yellow : .gray)
+                            VStack {
                                 Text(self.isSubscribed ? "SUBSCRIBED".localized : "NOT_SUBSCRIBED".localized)
                                     .font(.headline)
-                                let leftDays = PaymentService.shared.expirationDaysFromToday()
-                                Text(self.isSubscribed ? "DAYS_LEFT".localizedFormat(leftDays) : "UPGRADE_TO_VIP".localized)
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
+                                    .foregroundStyle(.primary)
+                                if self.isSubscribed {
+                                    let dataString = PaymentService.shared.formartedExpirationDate()
+                                    Text("EXPIRES_ON".localizedFormat(dataString))
+                                        .font(.system(size: 14))
+                                        .foregroundStyle(.secondary)
+                                } else {
+                                    Text("UPGRADE_TO_VIP".localized)
+                                        .font(.system(size: 14))
+                                        .foregroundStyle(.secondary)
+                                }
                             }
-                            .padding(.leading, 5)
                         }
-                        .padding(0)
-                        .background(.clear)
+                        .padding(.leading, 20)
+                        Spacer()
+                        
+                        Image("viplogo")
+                            .padding(.trailing, 20)
                     }
-                    .padding(.horizontal, 20)
-                    .padding(.top, 0)
-                    .padding(.bottom, 20)
-                    Spacer()
-                }
-                
-                PaymentPlanView()
-                
-                VStack(spacing: 12) {
-    //                Divider()
-    //                    .padding(.vertical, 8)
                     
-                    // 会员服务说明
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("MEMBER_BENEFITS".localized)
-                            .font(.headline)
+                    HStack {
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("MEMBER_STATUS".localized)
+                                .font(.headline)
+                                .foregroundStyle(
+                                                LinearGradient(
+                                                    colors: [.init(hex: 0xABED3B), .init(hex: 0x61DFC2)],
+                                                    startPoint: .leading,
+                                                    endPoint: .trailing
+                                                )
+                                )
+                                .padding(.bottom, 0)
                         
-                        HStack {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                            Text("BENEFIT_1".localized)
-                        }
-                        
-                        HStack {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                            Text("BENEFIT_2".localized)
-                        }
-                        
-                        HStack {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                            Text("BENEFIT_3".localized)
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    .padding(.vertical, 20)
-                    .background(Color.init(hex: 0xF8FFEC))
-                    .cornerRadius(15)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(
-                                    Color.init(hex: 0x6CEACF), lineWidth: 1)
-                    )
-                    
-                    Spacer()
-                    
-                    VStack(spacing: 10) {
-                        Button(action: {
-                            guard let plan = mainStore.state.paymentState.selectedPlan else {
-                                fatalError("no selected plan")
+                            ZStack(alignment: .leading) {
+                                Image("vipstate")
+                                    .frame(width: 175, height: 114)
+                                
+                                VStack(alignment: .leading, spacing: 5) {
+                                    HStack {
+                                        Image("diamon")
+                                        Text("MEMBER_TYPE".localized)
+                                            .font(.system(size: 10))
+                                            .foregroundColor(.init(hex: 0xEEA47D))
+                                    }
+                                    .padding(.vertical, 5)
+                                    .padding(.horizontal, 10)
+                                    .background(Color.black)
+                                    .cornerRadius(15)
+                                    
+                                    Text(self.isSubscribed ? "SUBSCRIBED".localized : "NOT_SUBSCRIBED".localized)
+                                        .font(.headline)
+                                    let leftDays = PaymentService.shared.expirationDaysFromToday()
+                                    Text(self.isSubscribed ? "DAYS_LEFT".localizedFormat(leftDays) : "UPGRADE_TO_VIP".localized)
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                }
+                                .padding(.leading, 5)
                             }
-                            mainStore.dispatch(AppAction.payment(.startPayment(plan)))
-                        }) {
+                            .padding(0)
+                            .background(.clear)
+                        }
+                        .padding(.horizontal, 20)
+                        .padding(.top, 0)
+                        .padding(.bottom, 20)
+                        Spacer()
+                    }
+                    
+                    VStack {
+                        PaymentPlanView()
+                            .frame(height: 120)
+                    }.padding(0)
+                    
+                    
+                    VStack(spacing: 12) {
+        //                Divider()
+        //                    .padding(.vertical, 8)
+                        
+                        // 会员服务说明
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("MEMBER_BENEFITS".localized)
+                                .font(.headline)
+                            
                             HStack {
-                                Image(systemName: "applelogo")
-                                Text("SUBSCRIBE_NOW".localized)
+                                Image(systemName: "checkmark.circle.fill")
+                                    .foregroundColor(.green)
+                                Text("BENEFIT_1".localized)
                             }
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.black)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
+                            
+                            HStack {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .foregroundColor(.green)
+                                Text("BENEFIT_2".localized)
+                            }
+                            
+                            HStack {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .foregroundColor(.green)
+                                Text("BENEFIT_3".localized)
+                            }
                         }
-                        .disabled(paymentIsProcessing)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                        .padding(.vertical, 20)
+                        .background(Color.init(hex: 0xF8FFEC))
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(
+                                        Color.init(hex: 0x6CEACF), lineWidth: 1)
+                        )
+                        
+                        Spacer()
+                        
+                        VStack(spacing: 10) {
+                            Button(action: {
+                                guard let plan = mainStore.state.paymentState.selectedPlan else {
+                                    fatalError("no selected plan")
+                                }
+                                mainStore.dispatch(AppAction.payment(.startPayment(plan)))
+                            }) {
+                                HStack {
+                                    Image(systemName: "applelogo")
+                                    Text("SUBSCRIBE_NOW".localized)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.black)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                            }
+                            .disabled(paymentIsProcessing)
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 40)
                     }
                     .padding(.horizontal)
-                    .padding(.bottom, 40)
                 }
-                .padding(.horizontal)
             }
+            
             
             if paymentIsProcessing {
                 Color.black.opacity(0.4)
